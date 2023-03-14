8bd9e3250ec588a62afb3b14e45335cec2bd81f6
Graziela França <38873168+Grazielafranca31@users.noreply.github.com>
2023-03-13 21:51:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ app=Flask(__name__)
 @app.route("/")
 def hello_world():
   return "Olá, mundo! Esse é o meu site. (Graziela França)"
+
+@app.route("/sobre")
+def sobre():
+  return "Aqui vai o conteúdo da página Sobre"
+
+@app.route("/contato")
+def contato():
+  return "Aqui vai o conteúdo da página Contato"
